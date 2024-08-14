b8daa048af9cdeb87160546ddccfbca576b28259
Min-Seok Kim <106315809+snumin44@users.noreply.github.com>
2024-08-14 21:34:50 +0900

    Update run_semantic_search.sh

--- example/run_semantic_search.sh
+++ example/run_semantic_search.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # If you wanna use more gpus, set GPU_ID like "0, 1, 2"
-# --bm25_path '../pickles/bm25_pickle.pkl' \
 
 GPU_ID=0
 CUDA_VISIBLE_DEVICES=$GPU_ID python3 inference.py \
     	--model '../pretrained_model/question_encoder' \
         --faiss_path '../pickles/faiss_pickle.pkl' \
         --context_path '../pickles/context_pickle.pkl' \
+        --bm25_path '../pickles/bm25_pickle.pkl' \
         --faiss_weight 1 \
         --bm25_weight 0.3 \
         --search_k 2000 \
