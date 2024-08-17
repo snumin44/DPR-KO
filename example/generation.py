5d6b20804195cc0cce4ae550f599499bf24ed610
Min-Seok Kim <106315809+snumin44@users.noreply.github.com>
2024-08-17 17:53:06 +0900

    Update generation.py

--- example/generation.py
+++ example/generation.py
@@ -137,6 +137,7 @@ class Generator(object):
             pad_token_id= self.tokenizer.eos_token_id,
             
             max_new_tokens=max_new_tokens,
+            num_bemas = num_beams,
             do_sample= do_sample,
             temperature=temperature,
             top_k = top_k,
